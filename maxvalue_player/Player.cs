d684ea670a6fe47f42e807166791e6c0060c9768
rsalo1981 <55482188+rsalo1981@users.noreply.github.com>
2019-09-29 17:41:23 +0200

    Add project files.

---
+++ maxvalue_player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maxvalue_player
+{
+    class Player
+    {
+        public string FirstName;
+        public string LastName;
+        public int Score;
+        /// <summary>
+        /// prints the details about players
+        /// </summary>
+        public void Printdetails()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has score: {Score}");
+        }
+
+    }
+}
---
+++ maxvalue_player/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace maxvalue_player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool game = true;
+            int max = 0;
+            List<Player> playerList = new List<Player>(); // list of player
+            List<int> scoreplayer = new List<int>();    // list player scores
+            Console.WriteLine("Welcome to this program!");
+            Console.ReadLine();
+
+            while (game)
+            {
+                Player players = new Player(); // object name is players from clas Player
+
+                Console.Write("Enter player first name: ");
+                string input = Console.ReadLine();
+                if (input == "n")
+                {
+                    game = false;
+                }
+                players.FirstName = input;  // input first name from user 
+                Console.Write("Enter player last name: ");
+                input = Console.ReadLine();
+                players.LastName = input;   // input last name from user
+                Console.Write("Enter player score:");
+                input = Console.ReadLine();     // input scores from users
+                players.Score = int.Parse(input);
+
+                playerList.Add(players);
+
+                Console.WriteLine("do you want to enter another player y/n");
+                input = Console.ReadLine(); // user enter y to continue or n to finish
+                if (input == "y")
+                {
+                    game = true;
+                }
+                else if (input == "n")
+                {
+                    game = false;
+                }
+                scoreplayer.Add(players.Score);
+                max = scoreplayer.Max();
+                scoreplayer.Sort();
+
+            }
+            // print details of users
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                playerList[i].Printdetails();
+
+            }
+            Console.WriteLine("max score is : " + scoreplayer.Max());
+        }
+    }
+}
